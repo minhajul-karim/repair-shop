4de557d4d9b110c151ec9c445b357e1ded02d3e4
Minhajul Karim <project@zaagsys.com>
2025-07-12 16:40:24 +0600

    feat: create the landing page & do setup for the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <Button>Click me</Button>
+    <section 
+      className="w-full h-dvh bg-gray-900 bg-center bg-cover flex justify-center items-center"
+      style={{
+        backgroundImage: "url('/assets/images/home_hero.jpg')"
+      }}
+    >
+      <div className="text-white bg-black w-fit p-10 flex flex-col gap-4">
+        <h1 className="text-4xl">Minhajul Karim</h1>
+        <address className="text-xl">
+          Munshi Bari <br />
+          Noorpur chowmuhony <br />
+          Cumilla City Corporation
+        </address>
+        <Link className="text-xl" href="tel:01711091062">01711091062</Link>
+      </div>
+    </section>
   );
 }
