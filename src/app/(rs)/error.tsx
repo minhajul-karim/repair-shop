1207327a62166aad0a03b7b20630a7976100e811
Minhajul Karim <project@zaagsys.com>
2025-07-27 20:07:16 +0600

    feat: add error page for the (rs) route group

---
+++ src/app/(rs)/error.tsx
@@ -0,0 +1,31 @@
+"use client"; // Error boundaries must be Client Components
+
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>RS: Something went wrong!</h2>
+      <button
+        className="cursor-pointer bg-amber-200 p-2 text-black rounded mt-2"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
